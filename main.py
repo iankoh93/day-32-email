1a14c76de021a943e30786f62f67f83fb6b47479
Ian Koh <ian_koh93@hotmail.com>
2022-04-07 16:26:53 -0700

    Finished the thursday quote email challenge.

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import smtplib
 import datetime as dt
+import random as r
 
 # ---------- Account Info ---------- #
 my_email = "ezonechess@gmail.com"
 password = "Testing123!"
 to_email = "ian_koh93@hotmail.com"
 
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs=to_email,
-#         msg="Subject:Hello\n\nThis is the body of my email!"
-#     )
-
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day = now.day
 day_of_week = now.weekday()
-print(f"{year}, {month}, {day} and {day_of_week} starting on monday as 0")
 
-date_of_birth = dt.datetime(year=1993, month=8, day=14)
-print(date_of_birth)
+with open("quotes.txt", 'r') as file:
+    quote = r.choice(file.readlines())
+
+if day_of_week == 3:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to_email,
+            msg=f"Subject:The Thursday Email Blast\n\nThe Quote of the day is:\n\n{quote}"
+        )
+
